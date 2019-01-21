d0960f36b4f745ddcf1fb385da346f0661bdc485
adz5a <antoine.d.zimmermann@gmail.com>
2019-01-21 19:36:10 +0100

    This should work but it does not ;(
    I implement the solution as a simple sum over the overlapping pieces.
    For each claim I add its pieces to the board and check the size of the
    board and compare it to the expected size of the board (sum of the
    pieces to be added with the currebt size of the board). I add the the
    difference of those two quantities.

--- src/adventofcode_2018/day3.clj
+++ src/adventofcode_2018/day3.clj
@@ -31,84 +31,41 @@
                   out))
         "Every claim is correctly parsed"))
 
-;; The idea is pretty simple but not really efficient from the complexity point
-;; of view:
-;; Had we had circles, detecting overlap would have been straightforward as it
-;; is a mere comparison of distance between the center of each circles to the
-;; sum of their respective radius.
-;; We cannot really do this unless we use some kind of special norm
-;; We say that rectangles are circles of radius 1 under the norm ||(x, y)|| =
-;; max(|x|, |y|) with (x, y) normalised with (respectively) the width and the
-;; height of the rectangles. We say that there is no overlap between two squares
-;; S1 S2 if every corner of S2 is at distance d > 1 of the center of S1. The
-;; center of S1 is the mean of the coordinates of each corners of S1.
+(def claims (parse-input-str test-data))
 
-(defn get-corners [claim]
-  (let [top-left [(:left claim) (:top claim)]
-        top-right [(+ (:width claim) (:left claim)) (:top claim)]
-        bottom-left [(:left claim) (+ (:height claim) (:top claim))]
-        bottom-right [(+ (:width claim) (:left claim)) (+ (:height claim) (:top claim))]]
-
-    [top-left
-     top-right
-     bottom-left
-     bottom-right]))
-
-(comment (println (get-corners (first (parse-input-str test-data)))))
-
-(defn abs
-  "x -> |x|"
-  [x]
-  (max (- x) x))
-
-(t/deftest test-abs
-  (t/is (= 1 (abs -1))))
-
-(defn get-center [claim]
-  (let [corners (get-corners claim)]
-    [(/ (reduce + 0 (map first corners)) 4)
-     (/ (reduce + 0 (map second corners)) 4)]))
-
-(comment
-  (let [[claim] (parse-input-str test-data)]
-      (get-center claim)))
-
-(defn get-norm
-  "Returns a function wich takes a point [x y]
-  and returns its distance to the claim passed
-  as argument. the distance is normalized with
-  the size of the claim such as its bounds are
-  at distance 1 of the center of the claim per
-  this function."
+(defn get-pieces
+  "Get all the pieces of a claim as a sequence of [int int]."
   [claim]
-  (let [{:keys [width height]} claim
-        w (/ width 2)
-        h (/ height 2)
-        [cx cy] (get-center claim)]
-    (fn dist-to-center [point]
-      (let [[x y] point
-            dx (abs (- x cx))
-            dy (abs (- y cy))]
-        (max (/ dx w)
-             (/ dy h))))))
+  (let [{:keys [left top width height]} claim]
+    (for [x (range width)
+          y (range height)]
+      [(+ x left)
+       (+ y top)])))
 
-(defn overlaps?
-  "tests id test-claim overlaps ref-claim. One
-  claim is said to overlap another if one of its
-  corners is at a normalized distance < 1 of the
-  center of ref-claim"
-  [ref-claim test-claim]
-  (let [dist-to-ref (get-norm ref-claim)
-        corners (get-corners test-claim)
-        dists (map dist-to-ref corners)]
-    (println dists)
-    (some (partial > 1) dists)))
+(defn count-overlapping
+  "Returns the number of overlapping pieces."
+  [claims]
+  (let [r (reduce
+            (fn [{:keys [board nb]} claim]
+              (let [pieces (get-pieces claim)
+                    board-size (count board)
+                    nb-pieces (count pieces)
+                    expected-size (+ board-size nb-pieces)
+                    next-board (apply conj board pieces)
+                    overlapping-pieces (- expected-size (count next-board))]
+                {:board next-board
+                 :nb (+ nb overlapping-pieces)}))
+            {:board #{}
+             :nb 0}
+            claims)]
+    (:nb r)))
+
+(comment (count-overlapping claims))
+
+(def data (slurp "src/adventofcode_2018/input.txt"))
+(def real-claims (parse-input-str data))
 
 (comment
-  (let [claims (parse-input-str test-data)]
-    (for [x claims
-          y claims
-          :when (and
-                  (not= x y)
-                  (overlaps? x y))]
-      [(:id x) (:id y)])))
+  (println data)
+  (println real-claims)
+  (println (count-overlapping (take 10 real-claims))))
